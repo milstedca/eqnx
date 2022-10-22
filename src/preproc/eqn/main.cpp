milstedca/eqnx

commit c2e894cf5b42cfdaae48f03476fee24a6103f9c9

[eqn]: Clear line number when it becomes nonsense.
* src/preproc/eqn/lex.cpp (get_delimited_text): Clear line number when
  hitting EOF.

* src/preproc/eqn/main.cpp: Revise end-of-file handling.  Clear line
  number upon reaching EOF so that we don't report a nonsense value one
  greater than the number of lines in the input file.

  (read_line): Boolify.  Make static (local linkage only), since it has
  no external callers.  Introduce new Boolean variable `is_end_of_file`.
  Clear `current_lineno` if EOF, and increment it otherwise.

  (do_file, main): Clarify diagnostic messages.

src/preproc/eqn/main.cpp
@@ -76,7 +76,7 @@ static const char *input_char_description(int c)
   return buf;
 }
 
-int read_line(FILE *fp, string *p)
+static bool read_line(FILE *fp, string *p)
 {
   p->clear();
   int c = -1;
@@ -88,8 +88,12 @@ int read_line(FILE *fp, string *p)
     if (c == '\n')
       break;
   }
-  current_lineno++;
-  return p->length() > 0;
+  bool is_end_of_file = (p->length() > 0);
+  if (is_end_of_file)
+    current_lineno++;
+  else
+    current_lineno = 0;
+  return is_end_of_file;
 }
 
 void do_file(FILE *fp, const char *filename)
@@ -132,7 +136,7 @@ void do_file(FILE *fp, const char *filename)
 	  else if (linebuf[2] == 'Q' && linebuf.length() > 3
 		   && (linebuf[3] == ' ' || linebuf[3] == '\n'
 		       || compatible_flag))
-	    fatal("nested .EQ");
+	    fatal("equations cannot be nested (.EQ within .EQ)");
 	}
 	str += linebuf;
       }
@@ -451,7 +455,7 @@ int main(int argc, char **argv)
 	errno = 0;
 	FILE *fp = fopen(argv[i], "r");
 	if (!fp)
-	  fatal("can't open '%1': %2", argv[i], strerror(errno));
+	  fatal("unable to open '%1': %2", argv[i], strerror(errno));
 	else {
 	  do_file(fp, argv[i]);
 	  if (fclose(fp) < 0)
@@ -459,7 +463,7 @@ int main(int argc, char **argv)
 	}
       }
   if (ferror(stdout))
-    fatal("error status on standard output stream");
+    fatal("standard output stream is in an error state");
   if (fflush(stdout) < 0)
     fatal("unable to flush standard output stream: %1",
 	  strerror(errno));

src/preproc/eqn/lex.cpp
@@ -710,6 +710,7 @@ void get_delimited_text()
     start = get_char();
   token_buffer.clear();
   if (start == EOF) {
+    current_lineno = 0;
     if (got_location)
       error_with_file_and_line(filename, lineno,
 			       "end of input while defining macro");
@@ -720,6 +721,7 @@ void get_delimited_text()
   for (;;) {
     int c = get_char();
     if (c == EOF) {
+      current_lineno = 0;
       if (got_location)
 	error_with_file_and_line(filename, lineno,
 				 "end of input while defining macro");

commit f49eea14745a72ded30ac9721a8401244cb6d7ed

[eqn]: Fix incorrect update of line number.
* src/preproc/eqn/main.cpp (read_line): Fix switched test consequents
  when updating `current_lineno`.

Problem introduced by me in commit c2e894cf5b, 16 October.

src/preproc/eqn/main.cpp
@@ -90,9 +90,9 @@ static bool read_line(FILE *fp, string *p)
   }
   bool is_end_of_file = (p->length() > 0);
   if (is_end_of_file)
-    current_lineno++;
-  else
     current_lineno = 0;
+  else
+    current_lineno++;
   return is_end_of_file;
 }
 
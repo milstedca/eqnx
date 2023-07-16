07f0e3fedbcf8f85b1d0d5bc08720b1e9b7fde7d
G. Branden Robinson <g.branden.robinson@gmail.com>
2023-07-16 05:40:14 -0500

    src/roff/groff/groff.cpp: Update version message.
    * src/roff/groff/groff.cpp (main): Update copyright notice and warranty
      disclaimer.  Bump year.  Lead with declaration as free software (join
      us now); specify GPL version and user's option to upgrade to later
      version.  Use a warranty disclaimer inspired by GNU Bison.  Drop
      mention of "COPYING" file, which is not installed by groff's "install"
      make(1) target and not sufficient information to aid users of groff
      packages prepared by distributors (which neverless are legally bound
      to provide licensing information via some means, and do so with high
      reliability).  After almost 35 years, we can have some hope that
      people have heard of the GNU GPL and can locate a copy of it without
      great difficulty.  Also drop use of term "subprogram".

--- src/roff/groff/groff.cpp
+++ src/roff/groff/groff.cpp
@@ -251,14 +251,15 @@ int main(int argc, char **argv)
     case 'v':
       vflag = 1;
       printf("GNU groff version %s\n", Version_string);
-      printf(
-	"Copyright (C) 2022 Free Software Foundation, Inc.\n"
-	"GNU groff comes with ABSOLUTELY NO WARRANTY.\n"
-	"You may redistribute copies of groff and its subprograms\n"
-	"under the terms of the GNU General Public License.\n"
-	"For more information about these matters, see the file\n"
-	"named COPYING.\n");
-      printf("\ncalled subprograms:\n\n");
+      puts(
+"Copyright (C) 2023 Free Software Foundation, Inc.\n"
+"This is free software, distributed under the terms of the GNU General"
+" Public\n"
+"License, version 3, or any later version, at your option.  There is NO"
+" warranty;\n"
+"not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE."
+      );
+      puts("\nprograms called:\n");
       fflush(stdout);
       // Pass -v to all possible subprograms
       commands[PRECONV_INDEX].append_arg(buf);

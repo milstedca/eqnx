milstedca/eqnx

commit 705be31107334c00f7d963cca5f9e896b91b1022

[pic]: Fix Savannah #64488 (`lf` request usage).
* src/preproc/pic/troff.cpp (troff_output::set_location): Do more
  parameter validation.  Don't generate the two-argument form of the
  `lf` request if the C string holding the new file name to write isn't
  a null pointer.  Also `assert()` this since it should be an invariant.

Fixes <https://savannah.gnu.org/bugs/?64488>.  Thanks to Bjarni Ingi
Gislason for the report.

src/preproc/pic/troff.cpp
@@ -554,22 +554,24 @@ void troff_output::dot(const position &cent, const line_type &lt)
 	   "\\v'%.3fi+%.2fm'"
 	   ".\n.sp -1\n",
 	   c.x,
-	   c.y, 
+	   c.y,
 	   DOT_AXIS);
   }
 }
 
 void troff_output::set_location(const char *s, int n)
 {
-  if (last_filename != 0 && strcmp(s, last_filename) == 0)
-    printf(".lf %d\n", n);
-  else {
+  assert(s != 0 /* nullptr */);
+  if ((s != 0 /* nullptr */) && (last_filename != 0 /* nullptr */)
+      && strcmp(s, last_filename) == 0) {
     printf(".lf %d %s\n", n, s);
     char *lfn = strdup(s);
-    if (0 == lfn)
+    if (0 /* nullptr */ == lfn)
       fatal("memory allocation failure while copying file name");
     last_filename = lfn;
   }
+  else
+    printf(".lf %d\n", n);
 }
 
 // Local Variables:

commit 76af4781348027740537e4f3e620af55f4e40efa

[pic]: Fix Savannah #64628 (incorrect `lf` usage).
* src/preproc/pic/troff.cpp (strsame): Add utility function for
  comparing two C strings for identical content, handling null pointer
  arguments as `strcmp()` does not.

  (troff_output::set_location): Refactor; write the two-argument form of
  the `lf` request if the current and last seen file names _don't_
  match.  Problem introduced by me in commit 705be31107, 29 July.  Also
  fix heap memory leak when repeatedly updating
  `troff_output::last_filename` member variable.

Fixes <https://savannah.gnu.org/bugs/?64628>.  Thanks to Bjarni Ingi
Gislason for the report.

src/preproc/pic/troff.cpp
@@ -568,17 +568,32 @@ void troff_output::dot(const position &cent, const line_type &lt)
   }
 }
 
+// We might consider putting this in libgroff.  We treat null pointers
+// like NaNs: they are incommensurable even with themselves.
+bool strsame(const char *s, const char *t)
+{
+  if ((s == 0 /* nullptr */) || (t == 0 /* nullptr */))
+    return false;
+  return (strcmp(s, t) == 0);
+}
+
 void troff_output::set_location(const char *s, int n)
 {
   assert(s != 0 /* nullptr */);
-  if ((s != 0 /* nullptr */) && (last_filename != 0 /* nullptr */)
-      && strcmp(s, last_filename) == 0) {
-    printf(".lf %d %s\n", n, s);
-    char *lfn = strdup(s);
-    if (0 /* nullptr */ == lfn)
-      fatal("memory allocation failure while copying file name");
-    last_filename = lfn;
+  bool update_file_name = false;
+  if (s != 0 /* nullptr */) {
+    if (!strsame(s, last_filename)) {
+      char *lfn = strdup(s);
+      if (0 /* nullptr */ == lfn)
+	fatal("memory allocation failure while copying file name");
+      if (last_filename != 0 /* nullptr */)
+	free(const_cast<char *>(last_filename));
+      last_filename = lfn;
+      update_file_name = true;
+    }
   }
+  if (update_file_name)
+    printf(".lf %d %s\n", n, s);
   else
     printf(".lf %d\n", n);
 }